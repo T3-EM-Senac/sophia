32cf06c6effd9810242ccfdc4d788bff6a7537d7
Scholante <sophiascholante@gmail.com>
2025-11-27 08:21:08 -0300

    oi

--- terceiro/spotify site/script.js
+++ terceiro/spotify site/script.js
@@ -1,12 +1,14 @@
 const elements = document.querySelectorAll('.fade');
 
+
 function checkFade() {
-    const trigger = window.innerHeight * 0.9;
-    elements.forEach(el => {
-        const rect = el.getBoundingClientRect().top;
-        if (rect < trigger) el.classList.add('show');
-    });
+const trigger = window.innerHeight * 0.9;
+elements.forEach(el => {
+const rect = el.getBoundingClientRect().top;
+if (rect < trigger) el.classList.add('show');
+});
 }
+
 
 window.addEventListener('scroll', checkFade);
 window.addEventListener('load', checkFade);
