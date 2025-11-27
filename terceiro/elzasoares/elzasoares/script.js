ccd6fdeb54b89611318d53b1ae9b282f5f4e9fb6
Scholante <sophiascholante@gmail.com>
2025-11-27 08:14:33 -0300

    savw

---
+++ terceiro/elzasoares/elzasoares/script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() {
+    let texto = document.getElementById("curiosidade");
+    texto.style.display = "block";
+    texto.innerText = "Elza Soares foi eleita a 'Voz do Milênio' pela BBC em 1999.";
+}
